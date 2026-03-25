add8447a004dceaad8f955ed74b70097d7313fab
anushkkavarshney <vikashvarshney33@gmail.com>
2026-03-26 01:39:41 +0530

    cust dashboard updated, view details section done

--- backend/index.js
+++ backend/index.js
@@ -41,7 +41,7 @@ let app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.use("/uploads", express.static("uploads"));
 app.use(
   cors({
     origin: "http://localhost:5173",
